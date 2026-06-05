ff7cfff6d930783e295a201d11de83c9c9713bcd
jiaying-l <lijiaying326@gmail.com>
2026-06-05 00:34:44 -0700

    updated db layout

--- createdb.sql
+++ createdb.sql
@@ -4,29 +4,43 @@ USE RiftArea;
 
 # card entity: create cards table
 CREATE TABLE CARD (
-	card_serial VARCHAR(12) PRIMARY KEY,
-    card_name VARCHAR(30) NOT NULL, 
-    card_subname VARCHAR(30), 
-    domain SET('fury', 'body', 'order', 'calm', 'mind', 'chaos') NOT NULL, 
+	id VARCHAR(12) PRIMARY KEY, 
+    collectorNumber LONG NOT NULL, 
+    `set` VARCHAR(3) NOT NULL, 
+    `name` VARCHAR(30) NOT NULL, 
+    `description` VARCHAR(50), 
+    `type` SET('unit', 'spell', 'gear', 'champion', 'signature', 'legend', 'battlefield', 'rune', 'token') NOT NULL, 
     rarity ENUM('common', 'uncommon', 'rare', 'epic', 'showcase') NOT NULL, 
-    card_type SET('unit', 'spell', 'gear', 'champion', 'signature', 'legend', 'battlefield', 'rune', 'token') NOT NULL, 
+    faction SET('fury', 'body', 'order', 'calm', 'mind', 'chaos', 'null') NOT NULL, 
+    # keywords
+    flavorText VARCHAR(120),
+    # tags
     
-    energy_cost INT(2) CHECK(0 <= energy <= 12), 
-    power_cost INT(2) CHECK(0 <= power <= 12),
-    might INT(2) CHECK(0 <= might <= 12)
+    # card stats
+    energy INT(2) CHECK(0 <= energy <= 12), 
+    might INT(2), 
+    `power` INT(2) CHECK(0 <= `power` <= 12)
 );
 
-# tag functional entity: create tags table for cards
+# card art relation
+CREATE TABLE CARDART (
+	card_id VARCHAR(12) NOT NULL UNIQUE, 
+	fullURL LONG NOT NULL, 
+    
+    FOREIGN KEY (card_id) REFERENCES CARD(id)
+);
+
+# card tags entity
 CREATE TABLE TAG (
-	tag_id INT PRIMARY KEY AUTO_INCREMENT,
-    tag_name VARCHAR(30) UNIQUE NOT NULL
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(30) NOT NULL
 );
 
-# card tags relational entity: relational table for tags of cards
-CREATE TABLE CardTag (
-	card_serial VARCHAR(12), 
+# card tag relation
+CREATE TABLE CARDTAG (
+	card_id VARCHAR(12), 
     tag_id INT, 
     
-    FOREIGN KEY (card_serial) REFERENCES CARD(card_serial),
-    FOREIGN KEY (tag_id) REFERENCES TAG(tag_id)
+    FOREIGN KEY (card_id) REFERENCES CARD(id), 
+    FOREIGN KEY (tag_id) REFERENCES TAG(id)
 );
